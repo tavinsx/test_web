f7b92533fd575bbde35c3a60137022d23f4d82a9
gustavo <qa.externo@fortics.com.br>
2025-06-10 19:16:35 -0300

    versel

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import ghPages from 'vite-plugin-gh-pages';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/test_web/',
-  plugins: [react(), ghPages()],
+  base: '/',
+  plugins: [react()],
 });
